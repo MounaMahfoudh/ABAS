c0b97ed7fc5a15eb5e6c41ea0d572e0f4baa0e23
Mouna Mahfoudh <mouna.mahfoudh@t-systems.com>
2021-06-16 10:18:47 +0200

    updated

--- src/test/java/com/abas/spiel/SpielTests.java
+++ src/test/java/com/abas/spiel/SpielTests.java
@@ -1,22 +1,18 @@
 package com.abas.spiel;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 @SpringBootTest
 class SpielTests {
-	
-    @BeforeTestClass
-    public static void setupHeadlessMode() {
-        System.setProperty("java.awt.headless", "false");
-    }
 
-    @Test
-    public void someTest() { }
+	@Autowired
+	MyCommandLineRunner commandLineRunner;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testMain() {
+		commandLineRunner.run();		
+	}	
 
 }
